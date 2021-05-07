1f4bc003a155d0b42ff1f0aa58259a596b352956
Geoff Rich <geoff.rich@alaskaair.com>
2021-05-07 15:46:45 -0700

    Add Auro demo page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import '@alaskaairux/design-tokens/dist/tokens/CSSCustomProperties.css';
+import '@alaskaairux/auro-button';
+
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <auro-button onClick={() => {setCount(count + 1)}}>Hello Auro</auro-button>
+      <p>Count: {count}</p>
     </div>
   );
 }
